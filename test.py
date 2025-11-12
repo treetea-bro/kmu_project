ce927a6ded61133434f7aa45e63d0daf887995d4
peco <sorpwjdafh@naver.com>
2025-11-12 15:08:09 +0900

    feat(v2): v2

--- test.py
+++ test.py
@@ -6,47 +6,18 @@ def run(playwright: Playwright) -> None:
     context = browser.new_context()
     page = context.new_page()
     page.goto("https://www.naver.com/")
+    page.locator(
+        "html > body:nth-child(2) > div:nth-child(3) > div:nth-child(1) > div > div:nth-child(3) > div > div > form:nth-child(2) > fieldset:nth-child(1) > div:nth-child(15) > input"
+    ).click()
+    page.get_by_role("combobox", name="검색어를 입력해 주세요").fill("sdafsdf")
+    page.locator(
+        "html > body:nth-child(2) > div:nth-child(3) > div:nth-child(1) > div > div:nth-child(3) > div > div > form:nth-child(2) > fieldset:nth-child(1) > button:nth-child(16)"
+    ).click()
     with page.expect_popup() as page1_info:
         page.locator(
-            "html > body:nth-child(2) > div:nth-child(3) > div:nth-child(1) > div > div:nth-child(5) > ul > li:nth-child(5) > a"
+            "html > body:nth-child(2) > div:nth-child(3) > div:nth-child(2) > div:nth-child(1) > div:nth-child(2) > div:nth-child(8) > div > div > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > a"
         ).click()
     page1 = page1_info.value
-    page1.locator(
-        "html > body:nth-child(2) > section:nth-child(2) > header > div:nth-child(2) > div > div > div > div > div > ul > li:nth-child(3) > a"
-    ).click()
-    page1.locator(
-        "html > body:nth-child(2) > div:nth-child(1) > header:nth-child(2) > div > div:nth-child(2) > div:nth-child(1) > div > ul > li:nth-child(5) > a"
-    ).click()
-    page1.locator(
-        "html > body:nth-child(2) > div:nth-child(1) > header:nth-child(2) > div > div:nth-child(2) > div > div > ul > li:nth-child(6) > a"
-    ).click()
-    page1.locator(
-        "html > body:nth-child(2) > div:nth-child(1) > div:nth-child(5) > div:nth-child(2) > div:nth-child(3) > div:nth-child(2) > div:nth-child(1) > div:nth-child(2) > div > div > div > div:nth-child(1) > ul:nth-child(1) > li:nth-child(1) > div > a:nth-child(3)"
-    ).click()
-    with page.expect_popup() as page2_info:
-        page.locator(
-            "html > body:nth-child(2) > div:nth-child(3) > div:nth-child(1) > div > div:nth-child(5) > ul > li:nth-child(3) > a"
-        ).click()
-    page2 = page2_info.value
-    page2.locator(
-        "html > body:nth-child(2) > ui-view:nth-child(1) > div > main:nth-child(2) > div:nth-child(3) > div:nth-child(1) > section > div:nth-child(1) > div:nth-child(2) > div > a:nth-child(3)"
-    ).click()
-    with page2.expect_popup() as page3_info:
-        page2.locator(
-            "html > body:nth-child(2) > ui-view:nth-child(1) > div > main:nth-child(2) > div:nth-child(3) > div:nth-child(1) > section > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > a:nth-child(1)"
-        ).click()
-    page3 = page3_info.value
-    with page.expect_popup() as page4_info:
-        page.locator(
-            "html > body:nth-child(2) > div:nth-child(3) > div:nth-child(1) > div > div:nth-child(5) > ul > li:nth-child(5) > a"
-        ).click()
-    page4 = page4_info.value
-    page4.locator(
-        "html > body:nth-child(2) > section:nth-child(2) > header > div:nth-child(2) > div > div > div > div > div > ul > li:nth-child(5) > a"
-    ).click()
-    page4.locator(
-        "html > body:nth-child(2) > div:nth-child(1) > div:nth-child(5) > div:nth-child(2) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > ul:nth-child(2) > li:nth-child(3) > div > div > div:nth-child(2) > a:nth-child(1)"
-    ).click()
 
     # ---------------------
     context.close()
